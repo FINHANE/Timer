531ce69945624272f5ea1cc7cf5bda49c8653684
Finhane ArturSa <136709894+FINHANE@users.noreply.github.com>
2023-12-28 15:08:50 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+//Written on Dec 2023/28th
+    //FINHANE ARTUR
+
+var time=document.getElementsByClassName("timer")
+var timings=60;
+var i=0;
+var myInterval = setInterval(Timeout, 3600);
+
+function Timeout(){
+    if ((timings*60-i) %60>=10) {
+    time[0].innerHTML=parseInt(`${(timings*60-i) /60}`) + ":" + `${(timings*60-i) %60}`;
+} 
+else {
+    time[0].innerHTML=parseInt(`${(timings*60-i) /60}`) + ":0" + `${(timings*60-i) %60}`;
+}
+
+i++;
+}
